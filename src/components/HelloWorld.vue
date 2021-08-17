c4371aa79b5b60ea4d1d4137d8c0fe9347c44a9a
Felix Stuart <felixjstuart@Outlook.com>
2021-08-17 10:18:48 -0400

    FIrst Commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="hello">
+    <input v-model="tag" />
+    <button @click="fetch(tag)">Fetch</button>
+    <h3>{{info}}</h3>
+  </div>
+</template>
+
+<script>
+import axios from "axios"
+export default {
+  name: 'Dashboard',
+  data() {
+    return {
+      info : null,
+      tag: ""
+    }
+  },
+  methods: {
+    fetch(tag) {
+      axios.get("http://localhost:8081/clash/"+tag)
+        .then(data=> {this.info = data.data})
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+p {
+  margin: 40px 0 0;
+}
+a {
+  color: #42b983;
+}
+</style>
